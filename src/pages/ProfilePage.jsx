9d98cc3ec736d5625c8b4f343a5f6fa21b6a472b
Alebs Gbolahan <alebsgd@gmail.com>
2024-06-02 15:22:56 +0100

    profile page completed

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,41 +1,72 @@
-import React, { useEffect, useState } from "react";
-import { getUser } from "../services/authservice";
-import "./profilepage.scss";
+import React from "react";
 import useAuth from "../config/hooks/useAuth";
+import "./profilepage.scss";
 
 const ProfilePage = () => {
-  const [userData, setUserData] = useState(null);
   const { user } = useAuth();
-
-  useEffect(() => {
-    console.log("user", user.id);
-    getUser(user.id).then((res) => {
-      setUserData(res?.data);
-    });
-  }, [user]);
-
-  if (!userData) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className="profile-page">
       <h1>Your Profile</h1>
       <div className="profile-details">
         <p>
-          <strong>First Name:</strong> {userData.firstName}
+          <strong>First Name:</strong> {user?.firstName}
         </p>
         <p>
-          <strong>Second Name:</strong> {userData.lastName}
+          <strong>Last Name:</strong> {user?.lastName}
         </p>
         <p>
-          <strong>Email:</strong> {userData.email}
+          <strong>Email:</strong> {user?.email}
         </p>
         <p>
           <strong>Joined:</strong>{" "}
-          {new Date(userData.createdOn).toLocaleDateString()}
+          {new Date(user?.createdOn).toLocaleDateString()}
         </p>
       </div>
+      {user?.favourite.length > 0 && (
+        <div className="favorite-meals">
+          <h2>Your Favorite Meals</h2>
+          <ul>
+            {user?.favourite.map((meal) => (
+              <li key={meal.id}>
+                {meal.mealName}
+                {""}
+                {" $" + meal?.mealPrice}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {user?.orders.length > 0 && (
+        <div className="order-history">
+          <h2>Your Order History</h2>
+          <ul>
+            {user?.orders.map((order) => (
+              <li key={order.id}>
+                <p>
+                  <strong>Order ID:</strong> {order.id}
+                </p>
+                <p>
+                  <strong>Order Date:</strong>{" "}
+                  {new Date(order.createdOn).toLocaleDateString()}
+                </p>
+                <p>
+                  <strong>Order Status:</strong> {order.orderStatus}
+                </p>
+                <p>
+                  <strong>Order Address:</strong> {order.orderAddress}
+                </p>
+                <p>
+                  <strong>Order Phone:</strong> {order.orderPhone}
+                </p>
+                <p>
+                  <strong>Meals:</strong> {order?.meals?.length || 0}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
